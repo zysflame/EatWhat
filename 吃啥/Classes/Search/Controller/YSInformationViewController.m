52727a20152196bb97a145b9e53693089bf4fc78
张永帅 <1042991093@qq.com>
2016-08-13 20:16:40 +0800

    1.1版本

--- 吃啥/Classes/Search/Controller/YSInformationViewController.m
+++ 吃啥/Classes/Search/Controller/YSInformationViewController.m
@@ -12,32 +12,25 @@
 #import "YSRecommandInfoStuffCell.h"
 #import "YSInfoSepsCell.h"
 
-
+#import "YSMenuDataModel.h"
 #import "YSResultInfoModel.h"
 
 @interface YSInformationViewController () <UITableViewDelegate,UITableViewDataSource>
-
-@property (nonatomic, weak) UIWebView *webView;
 
 
 @property (nonatomic, weak) UITableView *tableView;
 
 /**  存放材料的数组*/
-@property (nonatomic,copy) NSArray *arrStuff;
+@property (nonatomic,copy) NSArray *arrMaterail;
 /**  存放操作步骤的数组 */
 @property (nonatomic,copy) NSArray *arrSteps;
 
-
-/** 制作时间*/
-@property (nonatomic,copy) NSString *strCookTime;
-/** Tips 小贴士*/
-@property (nonatomic,copy) NSString *strTips;
 /** intro 介绍*/
-@property (nonatomic,copy) NSString *strIntro;
+@property (nonatomic,copy) NSString *strMenuState;
 /** cover 图片*/
-@property (nonatomic,copy) NSString *strCover;
+//@property (nonatomic,copy) NSString *strMenuPic;
 /** title 食物名字*/
-@property (nonatomic,copy) NSString *strTitle;
+@property (nonatomic,copy) NSString *strMenuName;
 
 @property (nonatomic,weak) UIImageView *imvHeader;
 
@@ -45,53 +38,65 @@
 
 @implementation YSInformationViewController
 
-- (void)loadView {
-    UIWebView *webView = [[UIWebView alloc] init];
-    //webView.frame = self.view.bounds;
-    self.view = webView;
-    self.webView = webView;
-}
-
-//- (void)viewWillAppear:(BOOL)animated{
-//    [self requestInformation];
-//}
-
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self loadDefaultSetting];
 }
 
+
 #pragma mark 加载默认设置
 - (void)loadDefaultSetting{
-    NSString *str = [NSString stringWithFormat:@"http://www.chufang001.com/menu/show/%@.html",self.strID];
-    NSURL *url = [NSURL URLWithString:str];
-    NSURLRequest *request = [NSURLRequest requestWithURL:url];
-    [self.webView loadRequest:request];
     
-//    self.title = self.strTitle;
-//    
-//    UITableView *tableView = [[UITableView alloc] initWithFrame:self.view.frame style:UITableViewStyleGrouped];
-//    tableView.delegate = self;
-//    tableView.dataSource = self;
-//    tableView.estimatedRowHeight = 50;
-//    tableView.contentInset = UIEdgeInsetsMake(0, 0, 50, 0);
-//    [self.view addSubview:tableView];
-//    self.tableView = tableView;
-//    
-//    //    YSInfoTableHeaderView *headerView = [YSInfoTableHeaderView new];
-//    UIImageView *imvHeader = [UIImageView new];
-//    imvHeader.bounds = CGRectMake(0, 0, 0, 250);
-//    tableView.tableHeaderView = imvHeader;
-//    self.imvHeader = imvHeader;
     
+    UITableView *tableView = [[UITableView alloc] initWithFrame:self.view.frame style:UITableViewStyleGrouped];
+    tableView.delegate = self;
+    tableView.dataSource = self;
+    tableView.estimatedRowHeight = 60;
+    tableView.contentInset = UIEdgeInsetsMake(0, 0, -30, 0);
+    [self.view addSubview:tableView];
+    self.tableView = tableView;
+    
+    //    YSInfoTableHeaderView *headerView = [YSInfoTableHeaderView new];
+    UIImageView *imvHeader = [UIImageView new];
+    imvHeader.bounds = CGRectMake(0, 0, 0, 250);
+    
+    tableView.tableHeaderView = imvHeader;
+    self.imvHeader = imvHeader;
 }
 
+- (void)viewWillAppear:(BOOL)animated{
+    //    [self sendRequest];
+    [self requestInformation];
+}
+
+#pragma mark  > 发送网络请求 <
 - (void)requestInformation{
-    NSString *str = [NSString stringWithFormat:@"http://www.chufang001.com/menu/show/%@.html",self.strID];
-    [[YSHTTPRequest sharedHTTPRequest] GETWithURL:str withParam:nil andRequestSuccess:^(id responseObject) {
-        NSLog(@">>>>%@",responseObject);
+    //    http://115.231.110.203/appapi/getMenuDetails.asp
+    //    appName=%E5%91%B3%E4%BB%96&keyCode=4F8F092F7F6D0DB304341DE43AE9F92E&&
+    NSString *strURL = @"http://115.231.110.203/appapi/getMenuDetails.asp";
+    NSString *strID = self.strID;
+    NSDictionary *dicPrograme = @{@"appName":@"味他",@"keyCode":@"4F8F092F7F6D0DB304341DE43AE9F92E",@"timeStamp":@"1470713633495",@"menuId":strID,@"insideCode":@"10013",@"appType":@"1",@"ipAddress":@"115.231.110.203",@"version":@"2.1.1"};
+    //    NSLog(@"ID>>>>%@",strID);
+    [[YSHTTPRequest sharedHTTPRequest] POSTWithURL:strURL withParam:dicPrograme andRequestSuccess:^(id responseObject) {
+        //        NSLog(@"请求成功>>>>%@",responseObject);
+        NSDictionary *dic = (NSDictionary *)responseObject;
+        YSMenuDataModel *menuDataModel = [YSMenuDataModel menuDataModelWithDictionary:dic[@"data"]];
+        NSURL *headerImvURL = [NSURL URLWithString:menuDataModel.menuPic];
+        //注:在block块中实现ui的操作,都要在主线程中操作
+        __weak typeof(self) weakSelf = self;
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [weakSelf.imvHeader sd_setImageWithURL:headerImvURL];
+            weakSelf.title = menuDataModel.menuName;
+            weakSelf.strMenuState = menuDataModel.menuState;
+            
+            weakSelf.arrMaterail = menuDataModel.menuMaterial;
+            weakSelf.arrSteps = menuDataModel.menuStep;
+            [weakSelf.tableView reloadData];
+        });
     } andRequestFailure:^(NSError *error) {
-        NSLog(@"请求失败");
+        NSLog(@">>>>失败%@",error);
+        // 提示框控制器
+        
         __weak typeof(self) weakSelf = self;
         UIAlertController *alertContorller = [UIAlertController alertControllerWithTitle:@"友情提示" message:@"网络不好，请耐心等待" preferredStyle:UIAlertControllerStyleAlert];
         UIAlertAction *alertAction = [UIAlertAction actionWithTitle:@"返回" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action){
@@ -104,7 +109,6 @@
         [alertContorller addAction:alertAction];
         [self presentViewController:alertContorller animated:YES completion:nil];
     }];
-
 }
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView{
@@ -114,7 +118,7 @@
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
     NSInteger num = 0;
     if (section == 0) {
-        num = self.arrStuff.count;
+        num = self.arrMaterail.count;
     }
     if(section == 1){
         num = self.arrSteps.count;
@@ -124,18 +128,17 @@
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     if (indexPath.section == 0) {
-        static NSString *strID = @"StuffCell";
+        static NSString *strID = @"MaterialCell";
         UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:strID];
         if (!cell) {
             cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleValue2 reuseIdentifier:strID];
         }
-        YSInfoStuffModel *stuffModel = self.arrStuff[indexPath.row];
-        cell.textLabel.text = [NSString stringWithFormat:@"%@",stuffModel.name];
-        cell.detailTextLabel.text = stuffModel.weight;
+        YSMenuMaterialModel *materialModel = self.arrMaterail[indexPath.row];
+        cell.textLabel.textAlignment = NSTextAlignmentLeft;
+        cell.textLabel.text = [NSString stringWithFormat:@"%@",materialModel.materialName];
+        cell.detailTextLabel.text = materialModel.materialWeight;
         return cell;
         
-        //        YSRecommandInfoStuffCell *stuffCell = [YSRecommandInfoStuffCell infoStuffCellWithTableView:tableView];
-        //        return stuffCell;
     }
     
     static NSString *strID = @"SepsCell";
@@ -143,15 +146,12 @@
     if (!cell) {
         cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:strID];
     }
-    YSInfoStepsModel *stepsModel = self.arrSteps[indexPath.row];
-    NSURL *imvURL = [NSURL URLWithString:stepsModel.StepPhoto];
-    [cell.imageView sd_setImageWithURL:imvURL];
+    YSMenuStepModel *stepModel = self.arrSteps[indexPath.row];
+    //    NSURL *imvURL = [NSURL URLWithString:stepModel.stepPic];
+    //    [cell.imageView sd_setImageWithURL:imvURL];
     cell.textLabel.numberOfLines = 0;
-    cell.textLabel.text = [NSString stringWithFormat:@"%ld %@",indexPath.row + 1,stepsModel.Intro];
+    cell.textLabel.text = [NSString stringWithFormat:@"%ld、%@",indexPath.row + 1,stepModel.stepState];
     return cell;
-    //    YSInfoSepsCell *cell = [YSInfoSepsCell sepsCellWithTableView:tableView];
-    //    return cell;
-    
 }
 
 - (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section{
@@ -162,13 +162,13 @@
     }
 }
 
-- (NSString *)tableView:(UITableView *)tableView titleForFooterInSection:(NSInteger)section{
-    if (section == 0) {
-        return self.strCookTime;
-    }else{
-        return self.strTips;
-    }
-}
+//- (NSString *)tableView:(UITableView *)tableView titleForFooterInSection:(NSInteger)section{
+//    if (section == 0) {
+//        return self.strCookTime;
+//    }else{
+//        return self.strTips;
+//    }
+//}
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
     if (indexPath.section == 0) {
